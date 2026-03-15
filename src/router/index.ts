faac2b3a466559bfdeb963e6934038c89fd875d9
hjc <hjc_0827@outlook.com>
2026-03-15 12:31:27 +0800

    build: Change package manage to npm

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory("/practice-workspace-hjc/admin-system-vue"),
   routes,
   scrollBehavior: () => ({ left: 0, top: 0 }),
 })
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/practice-workspace-hjc/admin-system-vue/",
   plugins: [vue()],
   resolve: {
     alias: {

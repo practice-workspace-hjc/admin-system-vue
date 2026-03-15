0f05a7863840111b4343c14f0014aa4a93c421ca
hjc <hjc_0827@outlook.com>
2026-03-15 12:50:57 +0800

    refactor: Changes msw startup environment

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -2,13 +2,10 @@ import { worker } from './browser'
 
 // 在开发环境启动 MSW
 export async function setupMock() {
-  if (import.meta.env.DEV) {
-    return worker.start({
-      onUnhandledRequest: 'bypass',
-      serviceWorker: {
-        url: `${import.meta.env.BASE_URL.endsWith("/") ? import.meta.env.BASE_URL : `${import.meta.env.BASE_URL}/`}mockServiceWorker.js`,
-      }
-    })
-  }
-  return Promise.resolve()
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+    serviceWorker: {
+      url: `${import.meta.env.BASE_URL.endsWith("/") ? import.meta.env.BASE_URL : `${import.meta.env.BASE_URL}/`}mockServiceWorker.js`,
+    }
+  })
 }
